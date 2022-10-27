3a4507ef8969bb0270f797e5767f495b5a193e5a
Carlos Vivar <carlos.vivarrios@unil.ch>
2022-10-27 14:32:47 +0200

    Each transformation contained in a singular method. More reusable transform structure.

--- COVID19_project/transform.py
+++ COVID19_project/transform.py
@@ -6,23 +6,7 @@ from pyspark.sql.types import TimestampType
 import pyspark.pandas as ps
 
 
-def calc_rolling_mean(df: DataFrame, temporal_window:int) -> DataFrame:
-    """Calcultation of rolling mean
-
-    :param df: Input Spark DataFrame.
-    :param temporal_window: The size of the window when calculating the rolling mean
-    :return: Transformed DataFrame.
-    """
-    psdf = df.pandas_api()
-    roll_series = psdf["difference_total_cases"].rolling(temporal_window).mean()
-    roll_series.name = "rolling_mean_total_cases"
-    
-    roll_psdf = ps.merge(psdf, roll_series, left_index=True, right_index=True, how="left")
-    roll_df = roll_psdf.to_spark()
-    
-    return roll_df
-
-def transform_date_to_datetime(date: datetime.date) -> datetime.datetime:
+def transform_item_date_to_datetime(date: datetime.date) -> datetime.datetime:
     """transform Date format to Datetime. 
     
     It calculates the minimum datetime possible and combine it with the date.
@@ -35,21 +19,70 @@ def transform_date_to_datetime(date: datetime.date) -> datetime.datetime:
     
     return min_datetime
 
-def calc_daily_difference(df: DataFrame) -> DataFrame:
+def transform_col_date_to_datetime(df: DataFrame, input_name: str, output_name: str) -> DataFrame:
+    """Transform column date into to column in datetime type.
+
+    :param df: Input Spark DataFrame.
+    :param input_name: Name of the column to update.
+    :param output_name: Name of the column where to store the result. If input_name is equal to output name, the column will be overwritten.
+    :return: Transformed Spark DataFrame.
+    """
+    
+    reg_transform_date_to_datetime = udf(lambda d: transform_item_date_to_datetime(d), TimestampType())
+    df = df.withColumn(output_name, reg_transform_date_to_datetime(input_name))
+    
+    return df
+
+def transform_col_string_to_date(df: DataFrame, input_name: str, output_name: str) -> DataFrame:
+    """Transform column date in string to column date in date type.
+    
+    :param df: Input Spark DataFrame.
+    :param input_name: Name of the column to update.
+    :param output_name: Name of the column where to store the result. If input_name is equal to output name, the column will be overwritten.
+    :return: Transformed Spark DataFrame.
+    """
+    
+    df = df.withColumn(output_name, to_date(input_name, 'yyyy-MM-dd'))
+        
+    return df
+
+
+def calc_daily_difference(df: DataFrame, input_name: str , output_name: str) -> DataFrame:
     """Calcultation of daily difference
 
     :param df: Input Spark DataFrame.
+    :param input_name: Name of the column to process.
+    :param output_name: Name of the column where to store the result. If input_name is equal to output name, the column will be overwritten.
+
     :return: Transformed DataFrame.
     """
     
     psdf = df.pandas_api()
-    diff_series = psdf["total_cases"].diff()
-    diff_series.name = "difference_total_cases"
+    diff_series = psdf[input_name].diff()
+    diff_series.name = output_name
     
     diff_psdf = ps.merge(psdf, diff_series, left_index=True, right_index=True, how="left")
     diff_df = diff_psdf.to_spark()
 
     return diff_df
+
+def calc_rolling_mean(df: DataFrame, temporal_window:int, input_name: str, output_name: str) -> DataFrame:
+    """Calcultation of rolling mean
+
+    :param df: Input Spark DataFrame.
+    :param temporal_window: The size of the window when calculating the rolling mean.
+    :param input_name: Name of the column to process.
+    :param output_name: Name of the column where to store the result. If input_name is equal to output name, the column will be overwritten.
+    :return: Transformed DataFrame.
+    """
+    psdf = df.pandas_api()
+    roll_series = psdf[input_name].rolling(temporal_window).mean()
+    roll_series.name = output_name
+    
+    roll_psdf = ps.merge(psdf, roll_series, left_index=True, right_index=True, how="left")
+    roll_df = roll_psdf.to_spark()
+    
+    return roll_df
 
 
 def transform_data(df: DataFrame) -> DataFrame:
@@ -59,14 +92,14 @@ def transform_data(df: DataFrame) -> DataFrame:
     :return: Transformed Spark DataFrame.
     """
     
-    df = df.withColumn("date", to_date("date", 'yyyy-MM-dd'))
-    
-    reg_transform_date_to_datetime = udf(lambda d: transform_date_to_datetime(d), TimestampType())
-    df = df.withColumn("datetime", reg_transform_date_to_datetime("date"))
+    df = transform_col_string_to_date(df, input_name="date", output_name="date")
+
+    df = transform_col_date_to_datetime(df, input_name="date", output_name="datetime")
     
     df = df.sort("datetime")
-    df = calc_daily_difference(df)
-
-    df = calc_rolling_mean(df, 7)
+    
+    df = calc_daily_difference(df, input_name="total_cases" , output_name="difference_total_cases" )
+    
+    df = calc_rolling_mean(df, 7, input_name="difference_total_cases", column_name="rolling_mean_total_cases")
     
     return df
